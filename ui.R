c415b4a8324871c1bc0447a930fe7e008169aee4
jwaitkevich <60396155+jwaitkevich@users.noreply.github.com>
2020-02-22 12:55:22 -0500

    Removed default text for cleaner code

--- ui.R
+++ ui.R
@@ -1,21 +1,10 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
-    # Application title
     titlePanel("NBA Per Game Stats from 2018-19 Season"),
 
-    # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             radioButtons("dist", "Statistic:",
@@ -29,7 +18,6 @@ shinyUI(fluidPage(
                         value = 10),
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
             plotOutput("distPlot"),
             textOutput("instructions")
